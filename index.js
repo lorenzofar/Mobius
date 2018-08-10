7ab85915b71a3b206b150420334813599c8a28fd
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-08-10 11:58:18 +0200

    Initial commit
    Added project structure and  minimal web server

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+
+// Start server
+var port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App listening on ${port}`));
