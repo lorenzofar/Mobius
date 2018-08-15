2fdce00586f259fe01484bfff7fed602b96b3e55
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-08-15 12:01:06 +0200

    Add script to load page fragments

---
+++ public/assets/js/fragmentLoader.js
@@ -0,0 +1,13 @@
+const BASE_FOLDER = "/assets/fragments/";
+
+const fragments_rel = {
+    /* Here we put fragments to be loaded for each element
+    e.g. #container: fragment.html */
+}
+
+$(document).ready(() => {
+    Object.keys(fragments_rel).forEach(r => {
+        let temp = null;
+        (temp = $(r)) && temp.load(BASE_FOLDER + fragments_rel[r]);
+    })
+});
