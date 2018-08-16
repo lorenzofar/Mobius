24e586bada5a73c73e01e9face2ab373568ecb95
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-08-16 15:59:47 +0200

    Add endpoint to get artist details

--- other/artistsRouter.js
+++ other/artistsRouter.js
@@ -12,4 +12,31 @@ router.get("/", (req, res) => {
     );
 });
 
+router.get("/:id", (req, res) => {
+    let id = req.params.id;
+    db.query(
+        `SELECT 
+            a.name,
+            a.bio, 
+            a.pic,
+            a.website, 
+            array_agg(row_to_json(row(e.id, e.name, e.dt))) AS events
+        FROM artists a 
+        LEFT JOIN relations r ON a.id = r.artist 
+        LEFT JOIN events e ON e.id = r.event 
+        WHERE a.id = ${id}
+        GROUP BY   
+            a.name,
+            a.bio, 
+            a.pic,
+            a.website
+            `,
+        (err, result) => {
+            if (err) res.status(500).json(null);
+            else if (!result.rowCount) res.status(404).json(null);
+            else res.status(200).json(result.rows[0]);
+        }
+    );
+});
+
 module.exports = router;
