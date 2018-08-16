4bc96cebdf263513792d773428b9121e784de4c5
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-08-16 15:51:26 +0200

    Add endpoint to get event details
    Along with event details is returned also the list of related artists

--- other/eventsRouter.js
+++ other/eventsRouter.js
@@ -29,4 +29,34 @@ router.get("/", (req, res) => {
     );
 });
 
+router.get("/:id", (req, res) => {
+    let id = req.params.id;
+    db.query(
+        `SELECT 
+            e.name,
+            e.description, 
+            e.dt,
+            e.location, 
+            e.kind,
+            e.pic,
+            array_agg(row_to_json(row(a.id, a.name, a.pic))) AS artists 
+        FROM events e 
+        LEFT JOIN relations r ON e.id = r.event 
+        LEFT JOIN artists a ON a.id = r.artist 
+        WHERE e.id = ${id}
+        GROUP BY   
+            e.name,
+            e.description, 
+            e.dt,
+            e.location, 
+            e.kind,
+            e.pic`,
+            (err, result) => {
+                if(err) res.status(500).json(null);
+                else if(!result.rowCount) res.status(404).json(null);
+                else res.status(200).json(result.rows[0]);
+            }
+    )
+})
+
 module.exports = router;
