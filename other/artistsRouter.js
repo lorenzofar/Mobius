afa608ad575db84c7c53227d9d1606e37f733ad2
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-08-17 17:55:59 +0200

    Fix retrieval of artists with no events

--- other/artistsRouter.js
+++ other/artistsRouter.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get("/", (req, res) => {
     db.query(
-        `SELECT a.id, a.name, a.pic, COUNT(r.id) AS events FROM artists a INNER JOIN relations r ON a.id = r.artist GROUP BY a.id, a.name, a.pic ORDER BY a.name`,
+        `SELECT a.id, a.name, a.pic, COUNT(r.id) AS events FROM artists a LEFT JOIN relations r ON a.id = r.artist GROUP BY a.id, a.name, a.pic ORDER BY a.name`,
         (err, result) => {
             if (err) res.status(500).json(null);
             else res.status(200).json(result.rows);
