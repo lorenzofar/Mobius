9defc4d320435239a04c55d257b0f641607816ed
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-08-23 17:08:19 +0200

    Use moment to provide events dates

--- other/datesRouter.js
+++ other/datesRouter.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const db = require("./dbManager");
 const qh = require("./queryHelper");
+const moment = require("moment");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-    db.query(
-        "SELECT DISTINCT DATE_TRUNC('day', dt) AS date FROM events ORDER BY date;",
-        (err, result) => {
-            if (err) res.status(500).json(null);
-            else {
-                let dates = result.rows.map(r => r.date);
-                res.status(200).json(dates);
-            }
-        }
-    );
+  db.query(
+    "SELECT DISTINCT DATE_TRUNC('day', dt) AS date FROM events ORDER BY date;",
+    (err, result) => {
+      if (err) res.status(500).json(null);
+      else {
+        let dates = result.rows.map(r => moment.utc(r.date).toISOString());
+        res.status(200).json(dates);
+      }
+    }
+  );
 });
 
 module.exports = router;
