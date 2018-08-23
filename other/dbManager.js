65f6326d4e6ca6c57e7bff377c2044d16164827b
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-08-23 17:07:20 +0200

    Fix parsing of pg timestamp values

--- other/dbManager.js
+++ other/dbManager.js
@@ -1,17 +1,20 @@
-const { Client } = require("pg");
+const pg = require("pg");
+var types = pg.types;
+types.setTypeParser(1114, stringValue => stringValue);
 
 const credentials = {
-    user: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
-    database: process.env.DBNAME,
-    host: process.env.DBHOST,
-    port: process.env.DBPORT
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DBNAME,
+  host: process.env.DBHOST,
+  port: process.env.DBPORT
 }; // Create config object according to ENV variables
 
-const client = new Client(credentials); // Initialize client
+const client = new pg.Client(credentials); // Initialize client
 
-client.connect() // Connect to database
-    .then(() => console.log("Connected")) // Handle succesfull connection
-    .catch((err) => console.log("Error connecting")); // Handle connection errors
+client
+  .connect() // Connect to database
+  .then(() => console.log("Connected")) // Handle succesfull connection
+  .catch(err => console.log("Error connecting")); // Handle connection errors
 
 module.exports = client;
