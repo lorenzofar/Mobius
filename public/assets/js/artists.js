e8435388ad429302d60b682d48f6f9e650c0989e
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-08-24 18:50:10 +0200

    Add artist details page

--- public/assets/js/artists.js
+++ public/assets/js/artists.js
@@ -1,9 +1,10 @@
 const ARTIST_CARD_FRAGMENT = "/assets/fragments/artistCard.html";
+const ARTIST_INFO_FRAGMENT = "/assets/fragments/artistInfo.html";
 
 $(document).ready(getArtists);
 $.addTemplateFormatter("ArtistsHrefFormatter", formatArtistHref);
 $.addTemplateFormatter("ArtistsEventCountFormatter", formatArtistsEventCount);
-
+$.addTemplateFormatter("ArtistWebsiteFormatter", formatArtistWebsite);
 function getArtists() {
   $.get(`/artists`)
     .done(parseData)
@@ -26,6 +27,23 @@ function formatArtistsEventCount(value, template) {
   return `${value} event${value == 1 ? "" : "s"}`;
 }
 
+/* ARTIST DETAILS PAGE */
+function getArtistData() {
+  let artist_id = parseQueryString(location.search).id;
+  if (!artist_id) location.replace("/pages/artists.html"); // If no artist is specified, fall back to the artists list
+  $.get(`/artists/${artist_id}`)
+    .done(parseArtistData)
+    .catch(handleError);
+}
+
+function parseArtistData(data) {
+  $(document).prop("title", data.name);
+  $("#artist-info").loadTemplate(ARTIST_INFO_FRAGMENT, data, {async: true});
+}
+
+function formatArtistWebsite(value, template) {
+  return `http://${value}`; //Add http to url
+}
 function handleError(err) {
   //TODO: Show error message
 }
