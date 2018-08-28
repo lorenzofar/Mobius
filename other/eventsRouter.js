5b890f561f190a2a0893624d5ae8e1c2d6693245
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-08-28 15:26:40 +0200

    Add fields filter in event endpoint

--- other/eventsRouter.js
+++ other/eventsRouter.js
@@ -3,20 +3,20 @@ const db = require("./dbManager");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-    let date = req.query && req.query.date;
-    let type = req.query && req.query.type;
-    let search_string = "";
-    date = date && date.split('T')[0];
-    if(date || type){
-        search_string += "WHERE ";
-        date = date && `DATE_TRUNC('day', e.dt) = '${date}'`;
-        type = type && `e.type = '${type.toLowerCase()}'`;
-        let search = [date, type].filter(i => i!=null && i!=""); // Only get valid parameters
-        search_string += search.reduce((s1,s2) => `${s1} AND ${s2}`); // combine them to build a query string
-    }
+  let date = req.query && req.query.date;
+  let type = req.query && req.query.type;
+  let search_string = "";
+  date = date && date.split("T")[0];
+  if (date || type) {
+    search_string += "WHERE ";
+    date = date && `DATE_TRUNC('day', e.dt) = '${date}'`;
+    type = type && `e.type = '${type.toLowerCase()}'`;
+    let search = [date, type].filter(i => i != null && i != ""); // Only get valid parameters
+    search_string += search.reduce((s1, s2) => `${s1} AND ${s2}`); // combine them to build a query string
+  }
 
-    db.query(
-        `SELECT 
+  db.query(
+    `SELECT 
             e.id,
             e.name,
             e.description,
@@ -35,41 +35,43 @@ router.get("/", (req, res) => {
             e.dt,
             e.location,
             e.type`,
-        (err, result) => {
-            if (err) res.status(500).json([]);
-            else res.status(200).json(result.rows);
-        }
-    );
+    (err, result) => {
+      if (err) res.status(500).json([]);
+      else res.status(200).json(result.rows);
+    }
+  );
 });
 
 router.get("/:id", (req, res) => {
-    let id = req.params.id;
-    db.query(
-        `SELECT 
-            e.name,
-            e.description, 
-            e.dt,
-            e.location, 
-            e.type,
-            e.pic,
-            array_agg(row_to_json(row(a.id, a.name, a.pic))) AS artists 
+  let id = req.params.id;
+  let qs = req.query;
+  let selector = `
+    e.name,
+    e.description,
+    e.dt,
+    e.location,
+    e.type,
+    e.pic`;
+  if (qs.fields)
+    selector = qs.fields
+      .split(",")
+      .map(f => `e.${f}`)
+      .join(",");
+  db.query(
+    `SELECT ${selector},
+        array_agg(row_to_json(row(a.id, a.name, a.pic))) AS artists 
         FROM events e 
         LEFT JOIN relations r ON e.id = r.event 
         LEFT JOIN artists a ON a.id = r.artist 
         WHERE e.id = ${id}
         GROUP BY   
-            e.name,
-            e.description, 
-            e.dt,
-            e.location, 
-            e.type,
-            e.pic`,
-        (err, result) => {
-            if (err) res.status(500).json([]);
-            else if (!result.rowCount) res.status(404).json([]);
-            else res.status(200).json(result.rows[0]);
-        }
-    )
-})
+            ${selector}`,
+    (err, result) => {
+      if (err) res.status(500).json([]);
+      else if (!result.rowCount) res.status(404).json([]);
+      else res.status(200).json(result.rows[0]);
+    }
+  );
+});
 
 module.exports = router;
