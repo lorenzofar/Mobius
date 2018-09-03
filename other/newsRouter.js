0f6241b6218113cc76ec38f78891f738314266b8
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-09-03 23:13:38 +0200

    Add fields filter to news endpoint

--- other/newsRouter.js
+++ other/newsRouter.js
@@ -4,8 +4,11 @@ const qh = require("./queryHelper");
 var router = express.Router();
 
 router.get("/", (req, res) => {
+    let q = req.query;
+    let fields = q.fields.split(",");
+    let columns = fields.length ? fields : ["*"];
     db.query(
-        qh.select(["*"], "news"),
+        qh.select(columns, "news"),
         (err, result) => {
             if (err) res.status(500).json(null);
             else res.status(200).json(result.rows);
