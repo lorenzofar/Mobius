db46c5a4bc140adc8229970bd96bbca1627ac84c
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-09-03 18:43:37 +0200

    Add query to order artists

--- other/artistsRouter.js
+++ other/artistsRouter.js
@@ -2,20 +2,27 @@ const express = require("express");
 const db = require("./dbManager");
 var router = express.Router();
 
+const sort_regex = /^(asc|desc|ASC|DESC)$/;
+
 router.get("/", (req, res) => {
-    db.query(
-        `SELECT a.id, a.name, a.pic, COUNT(r.event) AS events FROM artists a LEFT JOIN performances r ON a.id = r.artist GROUP BY a.id, a.name, a.pic ORDER BY a.name`,
-        (err, result) => {
-            if (err) res.status(500).json(null);
-            else res.status(200).json(result.rows);
-        }
-    );
+  let q = req.query;
+  let sort = "";
+
+  if (q && q.sort && sort_regex.test(q.sort)) sort = q.sort;
+
+  db.query(
+    `SELECT a.id, a.name, a.pic, COUNT(r.event) AS events FROM artists a LEFT JOIN performances r ON a.id = r.artist GROUP BY a.id, a.name, a.pic ORDER BY a.name ${sort}`,
+    (err, result) => {
+      if (err) res.status(500).json(null);
+      else res.status(200).json(result.rows);
+    }
+  );
 });
 
 router.get("/:id", (req, res) => {
-    let id = req.params.id;
-    db.query(
-        `SELECT 
+  let id = req.params.id;
+  db.query(
+    `SELECT 
             a.name,
             a.bio, 
             a.pic,
@@ -31,12 +38,12 @@ router.get("/:id", (req, res) => {
             a.pic,
             a.website
             `,
-        (err, result) => {
-            if (err) res.status(500).json(null);
-            else if (!result.rowCount) res.status(404).json(null);
-            else res.status(200).json(result.rows[0]);
-        }
-    );
+    (err, result) => {
+      if (err) res.status(500).json(null);
+      else if (!result.rowCount) res.status(404).json(null);
+      else res.status(200).json(result.rows[0]);
+    }
+  );
 });
 
 module.exports = router;
