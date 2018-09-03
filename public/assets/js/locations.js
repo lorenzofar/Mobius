75f5e4ab280e074188531c7df949c071bb6775da
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-09-03 18:09:38 +0200

    Fix loading of locations

--- public/assets/js/locations.js
+++ public/assets/js/locations.js
@@ -1,27 +1,28 @@
 const MAP_CENTER = { lat: 45.465, lon: 9.186 }; // Coordinates of Milan
+var locations;
 
-function loadData() {
+$(document).ready(() => {
   $.get("/locations")
     .done(parseLocations)
     .catch(handleError);
-}
+});
 
 function parseLocations(data) {
+  locations = data;
   data.forEach(l =>
     $("#locations-container").loadTemplate($("#locationCardTemplate"), l, {
       append: true
     })
   );
-  pinLocations(data);
 }
 
-function pinLocations(data) {
+function pinLocations() {
   let map = new Microsoft.Maps.Map(document.getElementById("map"), {
     showMapTypeSelector: false,
     center: new Microsoft.Maps.Location(MAP_CENTER.lat, MAP_CENTER.lon)
   });
   map.setView({ mapTypeId: Microsoft.Maps.MapTypeId.grayscale });
-  data.forEach(l => {
+  locations.forEach(l => {
     if (l && l.lat && l.lon) {
       var latLon = new Microsoft.Maps.Location(l.lat, l.lon);
       var pushpin = new Microsoft.Maps.Pushpin(latLon, {
