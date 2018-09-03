3bff42a0c15d18e9c11253d6d6ba42c5de09ffb1
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-09-03 23:12:22 +0200

    Add fields filter to locations endpoint

--- other/locationsRouter.js
+++ other/locationsRouter.js
@@ -4,18 +4,23 @@ const qh = require("./queryHelper");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  db.query(qh.select(["*"], "locations"), (err, result) => {
+  let q = req.query;
+  let fields = q.fields.split(",");
+  let columns = fields.length ? fields : ["*"];
+  db.query(qh.select(columns, "locations"), (err, result) => {
     if (err) res.status(500).json([]);
     else {
       let out = result.rows.map(r => {
-        r.directions = r.directions.map(d => {
-          let st = d.split("~");
-          let o = {
-            "transport": st[0],
-            "directions": st[1]
-          };
-          return o;
-        });
+        if (r.directions) {
+          r.directions = r.directions.map(d => {
+            let st = d.split("~");
+            let o = {
+              transport: st[0],
+              directions: st[1]
+            };
+            return o;
+          });
+        }
         return r;
       });
       res.status(200).json(out);
