10b6d770aa9952d8aaed2acb4215a2129f11b176
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-09-04 10:20:02 +0200

    Add fields filter to info endpoint

--- other/informationRouter.js
+++ other/informationRouter.js
@@ -4,14 +4,22 @@ var router = express.Router();
 
 router.get("/", (req, res) => {
   let info = require("./info.json");
-  db.query("SELECT * FROM sponsors", (err, result) => {
-    let status = 200;
-    if (err) {
-      status = 500;
-      info["sponsors"] = [];
-    } else info["sponsors"] = result.rows;
-    res.status(status).json(info);
-  });
+  let out = {};
+  let q = req.query;
+  let fields = q && q.fields ? q.fields.split(",") : [];
+  fields.filter(f => f != "sponsors").forEach(f => (out[f] = info[f]));
+  if (fields.indexOf("sponsors") != -1)
+    db.query("SELECT * FROM sponsors", (err, result) => {
+      let status = 200;
+      if (err) {
+        status = 500;
+        out["sponsors"] = [];
+      } else out["sponsors"] = result.rows;
+      res.status(status).json(out);
+    });
+  else {
+    res.status(200).json(out);
+  }
 });
 
 module.exports = router;
